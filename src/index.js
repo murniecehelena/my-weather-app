03160fbc8da4b9c05008c5d9a25268e19525144d
murniecehelena <murniecehelena@gmail.com>
2021-03-21 23:59:05 +0200

    fixed the forecast

--- src/index.js
+++ src/index.js
@@ -31,11 +31,8 @@ let days = [
 
 let date = document.querySelector("#date");
 let time = document.querySelector("#time");
-let first = document.querySelector("#first");
-let second = document.querySelector("#second");
-let third = document.querySelector("#third");
-let fourth = document.querySelector("#fourth");
-let fifth = document.querySelector("#fifth");
+
+
 
 let now = new Date();
 let currentDay = days[now.getDay()];
@@ -55,12 +52,19 @@ if (currentMonth<12){
 
 date.innerHTML = `${currentDay}, ${currentDate}/${currentMonth}`
 time.innerHTML= `${hours}:${minutes}`
-first.innerHTML= days[now.getDay()+1];
-second.innerHTML=days[now.getDay()+2];
-third.innerHTML=days[now.getDay()+3];
-fourth.innerHTML=days[now.getDay()+4];
-fifth.innerHTML=days[now.getDay()+5];
- 
+
+ function formatHours(timestamp){
+  let now = new Date(timestamp);
+let hours = now.getHours();
+if (hours<10) {
+    hours = `0${hours}`
+}
+let minutes = now.getMinutes();
+if (minutes<10) {
+    minutes=`0${minutes}`
+}
+return `${hours}:${minutes}`;
+};
 
 
 
@@ -74,13 +78,13 @@ document.querySelector("#weatherDescription").innerHTML =response.data.weather[0
 if (response.data.rain!==undefined){
   document.querySelector("#precipitation").innerHTML = response.data.rain["1h"]+"mm/h";
 }else{
-  document.querySelector("#precipitation").innerHTML="no rain";
+  document.querySelector("#precipitation").innerHTML="no rain";};
   document.querySelector("#icon").setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
-};
+
 celsiusTemperature = response.data.main.temp;
 };
 
@@ -98,7 +102,29 @@ function showCelsius(event){
   celsiusLink.classList.add("active");
 fahrenheitLink.classList.remove("active");
   document.querySelector("#temperature").innerHTML =Math.round(celsiusTemperature);
+};
+
+function displayForecast(response){
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML=null;
+  let forecast = null;
+for (let index = 0; index < 5; index++) {
+  forecast = response.data.list[index];
+    forecastElement.innerHTML+=`
+                       <div class="col mx-1">
+                                        <div class="DayContainer-Element center">
+                                            <p class="AlignText-Center WeekDays" id="first">${formatHours(forecast.dt * 1000)}</p>
+                                            <div class="DayContainer-ElementCircle center">
+                                                <p class="AlignText-Center Temperature" id="FirstTemperature">${Math.round(forecast.main.temp_max)}/${Math.round(forecast.main.temp_min)}</p>
+                                                <img src="https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" class=" Icon" id="iconOne"></p>
+                                            </div>
+                                        </div>
+                          </div>
+                                    `;
 }
+  
+  };
+
 
 let celsiusTemperature=null;
 
@@ -111,6 +137,7 @@ fahrenheitLink.addEventListener("click", showFahrenheit );
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsius );
 
+
 function search(event){
 event.preventDefault();
 let apiKey = "76f9f2909bf68e0c3cbd1d951b779a49";
@@ -119,6 +146,7 @@ let city= document.querySelector("#search-form").value;
 let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`
 axios.get(apiUrl).then(displayWeatherConditions);
 
-apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
+ apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
 axios.get(apiUrl).then(displayForecast);
-};
+
+}
